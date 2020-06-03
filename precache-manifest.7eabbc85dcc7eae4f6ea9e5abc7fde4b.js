82e25012fd6e68adc7887b97a8ac2cac1972b23d
Shubham Patane <pataneshubham1997@gmail.com>
2020-06-03 08:32:16 +0530

    Updates

---
+++ precache-manifest.7eabbc85dcc7eae4f6ea9e5abc7fde4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd051dfddcabab0e8a46c63ba983dd09",
+    "url": "/github_user_search/index.html"
+  },
+  {
+    "revision": "688c7dac9cee430d0f48",
+    "url": "/github_user_search/static/css/main.d822dd09.chunk.css"
+  },
+  {
+    "revision": "839c757edfe2075df97e",
+    "url": "/github_user_search/static/js/2.5015d7b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github_user_search/static/js/2.5015d7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688c7dac9cee430d0f48",
+    "url": "/github_user_search/static/js/main.02dca552.chunk.js"
+  },
+  {
+    "revision": "3f7275a23894daf24b0b",
+    "url": "/github_user_search/static/js/runtime-main.339c56b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_user_search/static/media/logo.5d5d9eef.svg"
+  }
+]);
